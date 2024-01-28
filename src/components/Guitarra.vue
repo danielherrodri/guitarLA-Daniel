7701ba33a52bf7c7775f5ea469b367b5cb197249
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-01-27 23:27:40 -0600

    Vídeo 52 - Eventos en Vue.js - Inline handlers y Method Handlers

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { ref } from 'vue'
+const numero = ref(0)
 const props = defineProps({
   guitarra: {
     type: Object,
     required: true
   }
 })
+
+const incrementar = () => {
+  alert('Diste click')
+  numero.value++
+}
 </script>
 
 <template>
@@ -18,9 +25,12 @@ const props = defineProps({
     </div>
     <div class="col-8">
       <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+      <p>{{ numero }}</p>
       <p>{{ guitarra.descripcion }}</p>
       <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
-      <button type="button" class="btn btn-dark w-100">Agregar al Carrito</button>
+      <button @click="incrementar" type="button" class="btn btn-dark w-100">
+        Agregar al Carrito
+      </button>
     </div>
   </div>
 </template>
